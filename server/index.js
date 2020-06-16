740ab48a7d9a4aa5b96a86ce77769c8570e39776
Alina Sul <48648714+Alina-sul@users.noreply.github.com>
2020-06-17 01:36:52 +0300

    adding mongoose

--- server/index.js
+++ server/index.js
@@ -3,13 +3,40 @@ const path = require('path');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const router = express.Router();
+
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb://0.0.0.0/my_database';
+
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+const db = mongoose.connection;
+const Schema = mongoose.Schema;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('connected', console.error.bind(console, 'MongoDB connected:'));
+db.on('open', console.error.bind(console, 'MongoDB open:'));
+
+const TaskSchema = new Schema({
+    id: Number,
+    date: Date,
+    content: {
+        type: String,
+        maxlength: 100
+    },
+    status: {
+        type: String,
+        enum: ['done', 'pending']
+    },
+    tag: String
+});
+
+const Tasks = mongoose.model('Tasks', TaskSchema );
+
+
+
+
 
 app.use(cors());
-
-
-
 app.use(express.static(path.join(__dirname, 'client/build')));
-
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
